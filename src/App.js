07d87d64e9748d9d299d594007a85d6c5d269b52
Adile Ashirova <ashirova.a.i.23@gmail.com>
2025-04-11 23:59:19 +0300

    Task-8

--- src/App.js
+++ src/App.js
@@ -2,32 +2,20 @@ import './App.css';
 import React from 'react';
 
 function App() {
-	return <div>
-<h1>7.1</h1>
-<div>
-		text
-	</div>
-
-<h1>7.2</h1>
-<div>
-		<p>text</p>
-    <p>text</p>
-    <p>text</p>
-    <p>text</p>
-	</div>
-
-  <h1>7.3</h1>
-<div>
-		<p class='k1234'>text</p>
-    <p class='k1234'>text</p>
-    <p class='k1234'>text</p>
-    <p class='k1234'>text</p>
-	</div>
-  
-
-
-</div>
+  return (
+    <ul>
+      <li>Item 1</li>
+      <li>Item 2</li>
+      <li>Item 3</li>
+      <li>Item 4</li>
+      <li>Item 5</li>
+      <li>Item 6</li>
+      <li>Item 7</li>
+      <li>Item 8</li>
+      <li>Item 9</li>
+      <li>Item 10</li>
+    </ul>
+  );
 }
-
 
 export default App;
