4d2124f4814e9219e305097222350f6822fd2acc
Adile Ashirova <ashirova.a.i.23@gmail.com>
2025-04-11 16:53:26 +0300

    Task-7

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import './App.css';
 import React from 'react';
 
 function App() {
 	return <div>
+<h1>7.1</h1>
+<div>
 		text
-	</div>;
+	</div>
+
+<h1>7.2</h1>
+<div>
+		<p>text</p>
+    <p>text</p>
+    <p>text</p>
+    <p>text</p>
+	</div>
+
+  <h1>7.3</h1>
+<div>
+		<p class='k1234'>text</p>
+    <p class='k1234'>text</p>
+    <p class='k1234'>text</p>
+    <p class='k1234'>text</p>
+	</div>
+  
+
+
+</div>
 }
+
 
 export default App;
