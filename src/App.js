fe7fa75a3aba6778edb615a2022a368843fa6d4e
Adile Ashirova <ashirova.a.i.23@gmail.com>
2025-04-12 01:05:45 +0300

    Task-24

--- src/App.js
+++ src/App.js
@@ -1,41 +1,33 @@
-import './App.css';
 import React from 'react';
 
 function App() {
-  // Массив с элементами для списка
-  const items = Array.from({ length: 10 }, (_, index) => (
-    <li key={index}>Item {index + 1}</li>
-  ));
+  // №1
+  const num1 = 3;
+  const num2 = 2;
+
+  // №2
+  const name = 'john';
+  const surname = 'smit';
+
+  // №3
+  const num = 4;
 
   return (
     <div>
-      <h1>23_1</h1>
-      {/* Список */}
-      <ul>{items}</ul>
+      {/* №1 */}
+      <div>
+        result: {num1 ** num2}
+      </div>
 
-      <h1>23_2</h1>
-      {/* Таблица */}
-      <table border="1">
-        <thead>
-          <tr>
-            <th>1,1</th>
-            <th>1,2</th>
-            <th>1,3</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>2,1</td>
-            <td>2,2</td>
-            <td>2,3</td>
-          </tr>
-          <tr>
-            <td>3,1</td>
-            <td>3,2</td>
-            <td>3,3</td>
-          </tr>
-        </tbody>
-      </table>
+      {/* №2 */}
+      <div>
+        result: {name + ' ' + surname}
+      </div>
+
+      {/* №3 */}
+      <div>
+        result: {Math.sqrt(num)}
+      </div>
     </div>
   );
 }
