812a30aa2db6d9929f944ce3425b63f2a193fd42
Adile Ashirova <ashirova.a.i.23@gmail.com>
2025-04-12 17:32:48 +0300

    Task-70

--- src/App.js
+++ src/App.js
@@ -1,85 +1,36 @@
 import React, { useState } from 'react';
 
 function App() {
-  // Задание 1: 3 радиокнопочки и абзац
-  const [selectedOption, setSelectedOption] = useState('');
+  const initialArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const [array, setArray] = useState(initialArray);
 
-  const handleOptionChange = (event) => {
-    setSelectedOption(event.target.value);
+  const handleInputChange = (index, event) => {
+    const newArray = [...array];
+    newArray[index] = Number(event.target.value);
+    setArray(newArray);
   };
 
-  // Задание 2: Выбор любимого языка программирования с похвалой
-  const [favoriteLanguage, setFavoriteLanguage] = useState('');
-
-  const handleLanguageChange = (event) => {
-    setFavoriteLanguage(event.target.value);
+  const calculateAverage = (arr) => {
+    const sum = arr.reduce((acc, currentValue) => acc + currentValue, 0);
+    return sum / arr.length;
   };
 
   return (
     <div>
-      {/* Задание 1 */}
-      <h2>Выберите одну из опций:</h2>
-      <input 
-        type="radio" 
-        name="option" 
-        value="Option 1" 
-        onChange={handleOptionChange} 
-        checked={selectedOption === 'Option 1'} 
-      />
-      <label>Option 1</label>
-
-      <input 
-        type="radio" 
-        name="option" 
-        value="Option 2" 
-        onChange={handleOptionChange} 
-        checked={selectedOption === 'Option 2'} 
-      />
-      <label>Option 2</label>
-
-      <input 
-        type="radio" 
-        name="option" 
-        value="Option 3" 
-        onChange={handleOptionChange} 
-        checked={selectedOption === 'Option 3'} 
-      />
-      <label>Option 3</label>
-
-      <p>Вы выбрали: {selectedOption}</p>
-
-      {/* Задание 2 */}
-      <h2>Выберите ваш любимый язык программирования:</h2>
-      <input 
-        type="radio" 
-        name="language" 
-        value="JavaScript" 
-        onChange={handleLanguageChange} 
-        checked={favoriteLanguage === 'JavaScript'} 
-      />
-      <label>JavaScript</label>
-
-      <input 
-        type="radio" 
-        name="language" 
-        value="Python" 
-        onChange={handleLanguageChange} 
-        checked={favoriteLanguage === 'Python'} 
-      />
-      <label>Python</label>
-
-      <input 
-        type="radio" 
-        name="language" 
-        value="Ruby" 
-        onChange={handleLanguageChange} 
-        checked={favoriteLanguage === 'Ruby'} 
-      />
-      <label>Ruby</label>
-
-      <p>Ваш любимый язык программирования: {favoriteLanguage}</p>
-
-      {favoriteLanguage === 'JavaScript' && <p>Вы выбрали отличный язык! JavaScript - это мощно!</p>}
+      <h2>Среднее арифметическое элементов массива:</h2>
+      <p>Среднее арифметическое: {calculateAverage(array)}</p>
+      
+      <h3>Редактирование элементов массива:</h3>
+      {array.map((item, index) => (
+        <div key={index}>
+          <label>Элемент {index + 1}:</label>
+          <input 
+            type="number" 
+            value={item} 
+            onChange={(event) => handleInputChange(index, event)} 
+          />
+        </div>
+      ))}
     </div>
   );
 }
