47ae76e3a810d74d8397cbc2333c4c6ec61736c5
Adile Ashirova <ashirova.a.i.23@gmail.com>
2025-04-12 17:24:52 +0300

    Task-68

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
 import React, { useState } from 'react';
 
 function App() {
-  const [ageGroup, setAgeGroup] = useState('');
+  // Задание 1: 3 радиокнопочки и абзац
+  const [selectedOption, setSelectedOption] = useState('');
 
-  const handleChange = (event) => {
-    setAgeGroup(event.target.value);
+  const handleOptionChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
+  // Задание 2: Выбор любимого языка программирования с похвалой
+  const [favoriteLanguage, setFavoriteLanguage] = useState('');
+
+  const handleLanguageChange = (event) => {
+    setFavoriteLanguage(event.target.value);
   };
 
   return (
     <div>
-      <select onChange={handleChange} value={ageGroup}>
-        <option value="">Выберите вашу возрастную группу</option>
-        <option value="0-12">0-12 лет</option>
-        <option value="13-17">13-17 лет</option>
-        <option value="18-25">18-25 лет</option>
-        <option value="25+">Старше 25 лет</option>
-      </select>
+      {/* Задание 1 */}
+      <h2>Выберите одну из опций:</h2>
+      <input 
+        type="radio" 
+        name="option" 
+        value="Option 1" 
+        onChange={handleOptionChange} 
+        checked={selectedOption === 'Option 1'} 
+      />
+      <label>Option 1</label>
 
-      {ageGroup && <p>Вы выбрали возрастную группу: {ageGroup}</p>}
+      <input 
+        type="radio" 
+        name="option" 
+        value="Option 2" 
+        onChange={handleOptionChange} 
+        checked={selectedOption === 'Option 2'} 
+      />
+      <label>Option 2</label>
+
+      <input 
+        type="radio" 
+        name="option" 
+        value="Option 3" 
+        onChange={handleOptionChange} 
+        checked={selectedOption === 'Option 3'} 
+      />
+      <label>Option 3</label>
+
+      <p>Вы выбрали: {selectedOption}</p>
+
+      {/* Задание 2 */}
+      <h2>Выберите ваш любимый язык программирования:</h2>
+      <input 
+        type="radio" 
+        name="language" 
+        value="JavaScript" 
+        onChange={handleLanguageChange} 
+        checked={favoriteLanguage === 'JavaScript'} 
+      />
+      <label>JavaScript</label>
+
+      <input 
+        type="radio" 
+        name="language" 
+        value="Python" 
+        onChange={handleLanguageChange} 
+        checked={favoriteLanguage === 'Python'} 
+      />
+      <label>Python</label>
+
+      <input 
+        type="radio" 
+        name="language" 
+        value="Ruby" 
+        onChange={handleLanguageChange} 
+        checked={favoriteLanguage === 'Ruby'} 
+      />
+      <label>Ruby</label>
+
+      <p>Ваш любимый язык программирования: {favoriteLanguage}</p>
+
+      {favoriteLanguage === 'JavaScript' && <p>Вы выбрали отличный язык! JavaScript - это мощно!</p>}
     </div>
   );
 }
