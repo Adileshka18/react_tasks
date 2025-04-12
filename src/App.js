0f7c6175ffb493ec6c3b9d9d96896d763213c5fc
Adile Ashirova <ashirova.a.i.23@gmail.com>
2025-04-12 15:23:15 +0300

    Task-47

--- src/App.js
+++ src/App.js
@@ -1,35 +1,16 @@
 import React from 'react';
+import { nanoid } from 'nanoid'; 
+import uuid from 'react-uuid';   
 
 function App() {
-  const users = [
-    { id: 1, name: 'user1', surn: 'surn1', age: 30 },
-    { id: 2, name: 'user2', surn: 'surn2', age: 31 },
-    { id: 3, name: 'user3', surn: 'surn3', age: 32 },
-  ];
+ 
+  const randomId = nanoid();  
+  const randomUuid = uuid();  
 
   return (
     <div>
-      <table border="1">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Surname</th>
-            <th>Age</th>
-          </tr>
-        </thead>
-        <tbody>
-          {/* Используем .map для рендеринга строк таблицы */}
-          {users.map((user) => (
-            <tr key={user.id}>
-              <td>{user.id}</td>
-              <td>{user.name}</td>
-              <td>{user.surn}</td>
-              <td>{user.age}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <p>Случайный ID (nanoid): {randomId}</p>
+      <p>Случайный UUID (react-uuid): {randomUuid}</p>
     </div>
   );
 }
