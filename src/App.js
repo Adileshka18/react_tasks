ca7c454673e8d7bdfca047811cd0068f4adb56fa
Adile Ashirova <ashirova.a.i.23@gmail.com>
2025-04-12 15:41:48 +0300

    Task-58

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
 import React, { useState } from 'react';
 
 function App() {
-  const [inputText, setInputText] = useState('');
+  // Задание 1: Конвертация возраста в год рождения
+  const [age, setAge] = useState('');
+  const handleAgeChange = (event) => {
+    setAge(event.target.value);
+  };
+  const birthYear = age ? new Date().getFullYear() - age : '';
+
+  // Задание 2: Конвертация температуры Фаренгейта в Цельсий
+  const [fahrenheit, setFahrenheit] = useState('');
+  const handleFahrenheitChange = (event) => {
+    setFahrenheit(event.target.value);
+  };
+  const celsius = fahrenheit ? ((fahrenheit - 32) * 5) / 9 : '';
 
   return (
     <div>
-      <input 
-        type="text" 
-        value={inputText} 
-        onChange={event => setInputText(event.target.value)} 
+      {/* Задание 1 */}
+      <input
+        type="number"
+        placeholder="Введите ваш возраст"
+        value={age}
+        onChange={handleAgeChange}
       />
-      <p>Количество символов: {inputText.length}</p>
+      <p>Ваш год рождения: {birthYear}</p>
+
+      {/* Задание 2 */}
+      <input
+        type="number"
+        placeholder="Введите градусы Фаренгейта"
+        value={fahrenheit}
+        onChange={handleFahrenheitChange}
+      />
+      <p>Температура в Цельсиях: {celsius}</p>
     </div>
   );
 }
