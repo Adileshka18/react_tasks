8cd12aa5b623f58bcad3b65adff472545f5f3679
Adile Ashirova <ashirova.a.i.23@gmail.com>
2025-04-13 12:57:53 +0300

    Task-106

--- src/App.js
+++ src/App.js
@@ -1,50 +1,36 @@
+// №1
 import React from 'react';
 import styled from 'styled-components';
 
-// Стилизованный компонент для div
-const StyledDiv = styled.div`
+const DIVA = styled.div`
+  width: 150px;
+  height: 150px;
+  background-color: yellow;
+  border: 2px solid;
+`;
+
+// №2
+const DIVB = styled.div`
+  width: 150px;
+  height: 150px;
+  background-color: green;
+  border: 3px solid;
+`;
+
+// №3
+const Container = styled.div`
   display: flex;
   justify-content: space-between;
-  width: 300px;
-  margin: 20px auto;
-  padding: 10px;
-  border: 2px solid #ccc;
-  text-align: center;
 `;
 
-// Стилизованный компонент для кнопки
-const StyledButton = styled.button`
-  padding: 10px;
-  font-size: 16px;
-  cursor: pointer;
-  background-color: #4CAF50;
-  color: white;
-  border: none;
-  border-radius: 5px;
-
-  &:disabled {
-    background-color: #ccc;
-    cursor: not-allowed;
-  }
-  
-  &:last-child {
-    background-color: #f44336; /* Красный для reset кнопки */
-  }
-`;
-
-function Block({ isFirstDisabled }) {
+function Block1() {
   return (
-    <StyledDiv>
-      {/* Первая кнопка с блокировкой, если isFirstDisabled = true */}
-      <StyledButton disabled={isFirstDisabled}>Button 1</StyledButton>
-
-      {/* Вторая кнопка, обычная */}
-      <StyledButton>Button 2</StyledButton>
-
-      {/* Третья кнопка типа reset */}
-      <StyledButton type="reset">Reset</StyledButton>
-    </StyledDiv>
+    <Container>
+      <DIVA />
+      <DIVB />
+      <DIVA />
+    </Container>
   );
 }
 
-export default Block;
+export default Block1;
