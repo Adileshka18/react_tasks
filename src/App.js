e6d2d0d50d78692336d842d1052d3d8833f29677
Adile Ashirova <ashirova.a.i.23@gmail.com>
2025-04-13 12:50:55 +0300

    Task-100

--- src/App.js
+++ src/App.js
@@ -1,42 +1,19 @@
 import React from 'react';
+import styles from './style';  
 
 function ButtonComponent() {
-  // Создаем объекты стилей
-  const containerStyle = {
-    display: 'flex',
-    gap: '10px',
-    padding: '20px',
-    backgroundColor: '#f4f4f4',
-    borderRadius: '8px'
-  };
-
-  const buttonStyle = {
-    padding: '10px 20px',
-    fontSize: '16px',
-    border: 'none',
-    backgroundColor: '#007bff',
-    color: 'white',
-    cursor: 'pointer',
-    borderRadius: '4px'
-  };
-
-  // Стили для hover эффекта
-  const buttonHoverStyle = {
-    backgroundColor: '#0056b3'
-  };
-
   return (
-    <div style={containerStyle}>
+    <div style={styles.container}>
       <button 
-        style={buttonStyle} 
-        onMouseEnter={(e) => e.target.style.backgroundColor = buttonHoverStyle.backgroundColor} 
-        onMouseLeave={(e) => e.target.style.backgroundColor = buttonStyle.backgroundColor}>
+        style={styles.button}
+        onMouseEnter={(e) => e.target.style.backgroundColor = styles.buttonHover.backgroundColor} 
+        onMouseLeave={(e) => e.target.style.backgroundColor = styles.button.backgroundColor}>
         Кнопка 1
       </button>
       <button 
-        style={buttonStyle} 
-        onMouseEnter={(e) => e.target.style.backgroundColor = buttonHoverStyle.backgroundColor} 
-        onMouseLeave={(e) => e.target.style.backgroundColor = buttonStyle.backgroundColor}>
+        style={styles.button}
+        onMouseEnter={(e) => e.target.style.backgroundColor = styles.buttonHover.backgroundColor} 
+        onMouseLeave={(e) => e.target.style.backgroundColor = styles.button.backgroundColor}>
         Кнопка 2
       </button>
     </div>
---
+++ src/style.js
@@ -0,0 +1,24 @@
+const styles = {
+    container: {
+      display: 'flex',
+      gap: '10px',
+      padding: '20px',
+      backgroundColor: '#f4f4f4',
+      borderRadius: '8px',
+    },
+    button: {
+      padding: '10px 20px',
+      fontSize: '16px',
+      border: 'none',
+      backgroundColor: '#007bff',
+      color: 'white',
+      cursor: 'pointer',
+      borderRadius: '4px',
+    },
+    buttonHover: {
+      backgroundColor: '#0056b3',
+    },
+  };
+  
+  export default styles;
+  
