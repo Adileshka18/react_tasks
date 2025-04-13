b5b5f40ed042588abe2cedba933ad7c6dd5f1ef1
Adile Ashirova <ashirova.a.i.23@gmail.com>
2025-04-13 12:32:33 +0300

    Task-81

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 
-function User() {
-  return <p>Этот пользователь активен.</p>;
+function Employee({ surname, name, patronymic, salary }) {
+  return (
+    <div style={{ border: '1px solid #ccc', padding: '10px', margin: '10px', borderRadius: '5px' }}>
+      <h2>{surname} {name} {patronymic}</h2>
+      <p>Зарплата: {salary} руб.</p>
+    </div>
+  );
 }
 
 function App() {
   return (
     <div>
-      <h1>Список пользователей</h1>
-      <User />
-      <User />
-      <User />
-      <User />
+      <h1>Список сотрудников</h1>
+      <Employee surname="Иванов" name="Иван" patronymic="Иванович" salary={50000} />
+      <Employee surname="Петров" name="Петр" patronymic="Петрович" salary={60000} />
+      <Employee surname="Сидоров" name="Алексей" patronymic="Алексеевич" salary={55000} />
     </div>
   );
 }
