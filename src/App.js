3f00b95b79c1d34cea3f9e8213723ffa367d31be
Adile Ashirova <ashirova.a.i.23@gmail.com>
2025-04-13 12:54:09 +0300

    Task-103

--- src/App.js
+++ src/App.js
@@ -1,32 +1,40 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+  width: 300px;
+  border: 2px solid brown;
+  padding: 10px;
+  text-align: center;
+`;
+
+
+const StyledP1 = styled.p`
+  color: orangered;
+  font-weight: bold;
+`;
+
+
+const StyledP2 = styled.p`
+  font-style: italic;
+  color: brown;
+`;
+
+
+const StyledP3 = styled.p`
+  background-color: orange;
+  font-weight: bold;
+  color: white;
+`;
+
 function App() {
-    return (
-      <div style={{
-        width: '300px',
-        border: '2px solid brown',
-        padding: '10px',
-        textAlign: 'center'
-      }}>
-        <p style={{
-          color: 'orangered',
-          fontWeight: 'bold'
-        }}>
-          text
-        </p>
-        <p style={{
-          fontStyle: 'italic',
-          color: 'brown'
-        }}>
-          text
-        </p>
-        <p style={{
-          backgroundColor: 'orange',
-          fontWeight: 'bold',
-          color: 'white'
-        }}>
-          text
-        </p>
-      </div>
-    );
-  }
-  
-  export default App;
+  return (
+    <StyledDiv>
+      <StyledP1>text</StyledP1>
+      <StyledP2>text</StyledP2>
+      <StyledP3>text</StyledP3>
+    </StyledDiv>
+  );
+}
+
+export default App;
