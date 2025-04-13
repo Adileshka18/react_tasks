d368f776f152601cc0353afc6bfde14686bf2bff
Adile Ashirova <ashirova.a.i.23@gmail.com>
2025-04-13 12:51:56 +0300

    Task-101

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
-import React from 'react';
-import styles from './style';  
-
-function ButtonComponent() {
-  return (
-    <div style={styles.container}>
-      <button 
-        style={styles.button}
-        onMouseEnter={(e) => e.target.style.backgroundColor = styles.buttonHover.backgroundColor} 
-        onMouseLeave={(e) => e.target.style.backgroundColor = styles.button.backgroundColor}>
-        Кнопка 1
-      </button>
-      <button 
-        style={styles.button}
-        onMouseEnter={(e) => e.target.style.backgroundColor = styles.buttonHover.backgroundColor} 
-        onMouseLeave={(e) => e.target.style.backgroundColor = styles.button.backgroundColor}>
-        Кнопка 2
-      </button>
-    </div>
-  );
-}
-
 function App() {
-  return (
-    <div>
-      <ButtonComponent />
-    </div>
-  );
-}
-
-export default App;
+    return (
+      <div style={{
+        width: '300px',
+        border: '2px solid brown',
+        padding: '10px',
+        textAlign: 'center'
+      }}>
+        <p style={{
+          color: 'orangered',
+          fontWeight: 'bold'
+        }}>
+          text
+        </p>
+        <p style={{
+          fontStyle: 'italic',
+          color: 'brown'
+        }}>
+          text
+        </p>
+        <p style={{
+          backgroundColor: 'orange',
+          fontWeight: 'bold',
+          color: 'white'
+        }}>
+          text
+        </p>
+      </div>
+    );
+  }
+  
+  export default App;
